d0606b51e09b46cd95c3ed1c76c501f76c3babf4
aimonansari06 <50113053+aimonansari06@users.noreply.github.com>
2020-12-09 14:52:59 +0500

    more updates

--- src/Room.js
+++ src/Room.js
@@ -4,21 +4,30 @@ import './Room.css';
 
 
 function Room() {
-  let [isLit, setLit] = useState(false);  
+  let [isLit, setLit] = useState(true);  
   let [age, setage]= useState(23);
- 
+  let [temp, settemp]=useState(72);
   return (
     <div className={`room ${isLit? "lit": "dark"}`}>
         The room is: {isLit? "lit" : "dark"}
         <br/>
         Age = {age}
         <br/>
-        <button onClick={()=>setLit(!isLit)}> Toggle light</button>
+    <button onClick={()=>setLit(!setLit)}> Off</button>
+        <button onClick={()=>setLit(!(!setLit))}> On</button>
         <br/>
         <button onClick={()=> {
-                console.log("Age updated");
+            console.log("Age updated");
                 setage(++age);
                     }}> Update age</button>
+        <br/>
+        Temperature = {temp}
+        <br/> 
+        <button onClick={()=>settemp(22)}> Change to celcius</button>  
+        <button onClick={()=>settemp(72)}> Change to Farhenite</button> 
+        <br/>
+        <button onClick={()=>settemp(++temp)}>+</button>
+        <button onClick={()=>settemp(--temp)}>-</button>
     </div>
   );
 }
